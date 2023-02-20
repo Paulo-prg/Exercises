c3089753f0376ac2c9909c98d4cda4152c17b330
paulinho Rosa <tatirosinha19@gmail>
2023-02-20 14:34:50 -0300

    Sons commit

--- main.java
+++ main.java
@@ -1,7 +1,55 @@
-public class main {
+import java.util.ArrayList;
+import java.util.List;
 
-    public static void main(String[] args) {
-        System.out.println("Meu nome é Paulo");
+public abstract class main {
+
+    public class NewMain {
+
+   
+        /**
+         * @param args
+         */
+        public void main(String[] args) {
+           
+            String nome;
+        double duracao;
+    
+        final NewMain nome, final double duracao) {
+            this.nome = nome;
+            this.duracao = duracao;
+        }
+    
+        public String toString() {
+            return nome + duracao;
+        }
     }
-
+    
+    public class Do extends Nota {
+        public Do(double duracao) {
+            super("C", duracao);
+        }
+    }
+    
+    public class Re extends Nota {
+        public Re(double duracao) {
+            super("D", duracao);
+        }
+    }
+    
+    
+    
+    public class Sustenido extends Nota {
+        public Sustenido(Nota nota) {
+            super(nota.toString().replace("b", "#"), nota.duracao);
+        }
+    }
+    
+    public class Bemol extends Nota {
+        public Bemol(Nota nota) {
+            super(nota.toString().replace("#", "b"), nota.duracao);
+        }
+    }
+        }
 }
+
+
